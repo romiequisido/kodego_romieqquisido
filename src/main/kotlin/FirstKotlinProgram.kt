32c844645c2025618d414cafc8716f519fcbbc01
romie quisido <rome.quisido@gmail.com>
2022-09-05 14:29:11 +0800

    7th commit

--- src/main/kotlin/FirstKotlinProgram.kt
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -3,11 +3,14 @@ fun main(){
      Author: Romie
      Date Created: 9/1/2022
      Filename: FirstKotlinProgram.kt
+     git remote add origin <github_repo>
+     1) git add <filename>
+     2) git commit -m"message"
+     3) git push -u origin master
     */
     print("Hello World!")
     print("Hello World!!!!!")
     print("Hello World!!!!!")
     print("Hello World4")
-
 
 }

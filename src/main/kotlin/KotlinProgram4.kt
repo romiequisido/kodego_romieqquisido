64d169adcfc56223a9580cb1244e0d1428c56f07
romie quisido <rome.quisido@gmail.com>
2022-09-13 00:43:52 +0800

    4th commit

--- src/main/kotlin/KotlinProgram4.kt
+++ src/main/kotlin/KotlinProgram4.kt
@@ -5,5 +5,16 @@ fun main(){
         println("Hello World!")
     }
 
+    var grade : Int = 75
+    if ( grade >=75 ){
+        println("You passed!!!")
+    }
+
+    //range
+    var idNumber : Int = 1
+    if( (idNumber>=1)&&( idNumber<=3 ) ) {
+        println("Valid id number")
+        println("SUCCESS!!!!")
+    }
     println("Line Ends")
 }
